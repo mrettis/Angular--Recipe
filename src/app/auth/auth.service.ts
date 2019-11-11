26d158076548ee9c94c0153a5f79444fd7297c76
Marlon Rettis <mrettis@hotmail.com>
2019-11-10 21:12:06 -0500

    console.loged signUp Costumer token

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+
+
+interface AuthResponseData{
+    kind: string;
+    idToken: string;    
+    email:  string; 
+    refreshToken: string;
+    expiresIn:  string;
+    localId: string;   
+}
+
+@Injectable({providedIn:"root"})
+export class AuthService {
+
+    constructor(private http: HttpClient){}
+
+    singUp(email: string, password: string){
+       return this.http
+.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDEMauo778vunx0v4OMDkYRaN0oZyao2c0',                                                                                                  
+    
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+       );
+   
+     }
+}
